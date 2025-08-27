f3755659f0dd7da7b6b1890af225cba3a61b2322
Rizky H Dev <rizkyhdev@gmail.com>
2025-08-27 09:04:14 +0800

    profile fields page

--- resources/views/student/profile.blade.php
+++ resources/views/student/profile.blade.php
@@ -1,4 +1,75 @@
 @extends('student.studentapp')
 @section('content')
 <!-- Main Content -->
+  <div class="row mb-3 ">
+      <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nama') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ auth()->user()->name }}" autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                         <div class="row mb-3">
+                            <label for="nip" class="col-md-4 col-form-label text-md-end">{{ __('NIP') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nip" type="text" class="form-control @error('nip') is-invalid @enderror" name="nip" value="{{ auth()->user()->nip }}" required autocomplete="nip" autofocus>
+
+                                @error('nip')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Alamat Email') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ auth()->user()->email }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                          <div class="row mb-3">
+                            <label for="jabatan" class="col-md-4 col-form-label text-md-end">{{ __('Jabatan') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="jabatan" type="jabatan" class="form-control @error('jabatan') is-invalid @enderror" name="jabatan" value="{{ auth()->user()->jabatan }}" required autocomplete="jabatan">
+
+                                @error('jabatan')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                          <div class="row mb-3">
+                            <label for="unit_kerja" class="col-md-4 col-form-label text-md-end">{{ __('Unit Kerja') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="unit_kerja" type="unit_kerja" class="form-control @error('unit_kerja') is-invalid @enderror" name="unit_kerja" value="{{ auth()->user()->unit_kerja }}" required autocomplete="unit_kerja">
+
+                                @error('unit_kerja')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+    </div>
+
 @endsection
