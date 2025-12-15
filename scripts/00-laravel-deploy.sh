852511c28b7e2a47de3e24943b2f19799bc6d78b
Rizky H Dev <rizkyhdev@gmail.com>
2025-12-16 03:56:02 +0800

    rend dply 8

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+
 # echo "Running composer and npm install"
 # composer global require hirak/prestissimo
 # composer install --no-dev --working-dir=/var/www/html
@@ -23,20 +37,20 @@
 
 
 
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Running migrations..."
+# php artisan migrate --force
 
 # Clear cache
 # php artisan optimize:clear
 
 
 # Cache the various components of the Laravel application
-echo "Caching components of the Laravel application..."
-php artisan config:cache
-echo "Caching events..."
-php artisan event:cache
-echo "Caching routes..."
-php artisan route:cache
-echo "Caching views..."
-php artisan view:cache
+# echo "Caching components of the Laravel application..."
+# php artisan config:cache
+# echo "Caching events..."
+# php artisan event:cache
+# echo "Caching routes..."
+# php artisan route:cache
+# echo "Caching views..."
+# php artisan view:cache
 
