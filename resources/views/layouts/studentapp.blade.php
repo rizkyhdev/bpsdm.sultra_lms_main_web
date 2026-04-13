be80ee5bb005cb563c1fc7791deca8349c8a1375
Rizky H Dev <rizkyhdev@gmail.com>
2026-04-13 09:21:08 +0800

    fixed student blank white

--- resources/views/layouts/studentapp.blade.php
+++ resources/views/layouts/studentapp.blade.php
@@ -14,9 +14,9 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
   <!-- custom css -->
-  <link href="{{ asset('./css/custom.css') }}" rel="stylesheet">
-  <link href="{{ asset('./css/custom-style.css') }}" rel="stylesheet">
-  <link href="{{ asset('./css/pelatihan.css') }}" rel="stylesheet">
+  <link href="{{ asset('./css/custom.css') }}?v={{ filemtime(public_path('css/custom.css')) }}" rel="stylesheet">
+  <link href="{{ asset('./css/custom-style.css') }}?v={{ filemtime(public_path('css/custom-style.css')) }}" rel="stylesheet">
+  <link href="{{ asset('./css/pelatihan.css') }}?v={{ filemtime(public_path('css/pelatihan.css')) }}" rel="stylesheet">
 
   <!-- Font Awesome for Icons (via CDN) -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
